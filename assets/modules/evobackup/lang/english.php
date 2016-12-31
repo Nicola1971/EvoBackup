f27f2bd79133ff66938457f6bf88b23c7a446658
Nicola Lambathakis <modxit@modx.it>
2016-12-31 16:01:56 +0100

    language english & italian

---
+++ assets/modules/evobackup/lang/english.php
@@ -0,0 +1,69 @@
+<?php
+// ---------------------------------------------------------------
+// :: EvoBackup
+// ----------------------------------------------------------------
+//
+// 	Short Description:
+//         Backup Evo files and compress into .zip for easy download.
+//
+//   Version:
+//         1.2
+//
+//   Created by:
+// 	    Nicola Lambathakis http://www.tattoocms.it/
+//
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+
+$_lang['modulename'] = 'EvoBackup';
+$_lang['backup'] = 'backup';
+//Manage Backups
+$_lang['manage_backup'] = 'Manage Backups';
+$_lang['manage_backup_descr'] = 'Download or delete previous backup archives';
+$_lang['backup_filename'] = 'Filename';
+$_lang['backup_filesize'] = 'File size';
+$_lang['backup_file_options'] = 'Options';
+$_lang['delete_backup'] = 'Delete Backup';
+$_lang['download_backup'] = 'Download Backup';
+//Generate Backup
+$_lang['generate_backup'] = 'Generate a new Backup Archive';
+$_lang['choose_backup'] = 'Choose Backup type:';
+$_lang['light_backup'] = 'Light Backup';
+$_lang['medium_backup'] = 'Medium Backup';
+$_lang['full_backup'] = 'Full Site Backup';
+//assets Backup
+$_lang['assets_backup'] = 'Assets Backup';
+$_lang['whole_assets'] = 'whole assets folder';
+$_lang['assets_user_folders'] = 'User Folders';
+$_lang['assets_elements_folders'] = 'Elements Folders';
+$_lang['assets_system_folders'] = 'System Folders';
+//manager backup
+$_lang['manager_backup'] = 'Manager Backup';
+$_lang['whole_manager'] = 'whole manager folder';
+$_lang['only_those_manager_files'] = 'Only those manager files and folders:';
+$_lang['manager_config_file'] = 'config file';
+$_lang['manager_themes'] = 'manager themes';
+//root backup
+$_lang['root_backup'] = 'Root files Backup';
+$_lang['root_backup_descr'] = 'Select additional root files to include in archive';
+//db backup
+$_lang['db_backup'] = 'Database Backup';
+$_lang['include_db_to_zip'] = 'include .sql database backup to zip ';
+// custom backup
+$_lang['custom_files_backup'] = 'Custom Files and Folders';
+//buttons
+$_lang['backup_button_text'] = 'Backup Now!';
+$_lang['help'] = 'Help';
+$_lang['settings_module'] = 'Settings';
+$_lang['check_all'] = 'Select All';
+//help
+$_lang['help_light_backup'] = '<p>This backup includes only required files, user files and db.<br/> Generates a smaller zip archive and requires less memory and resources</p>  ';
+$_lang['help_medium_backup'] = '<p>This backup includes required files, user files, elements (snippets, modules, plugins..) and db.<br/> Generates a medium zip archive</p> ';
+$_lang['help_full_backup'] = '<p>This backup includes whole assets and manager folder (included custom sub folders), root files and db backup<br/> Generates a bigger zip archive and requires more memory and resources</p> </p>';
+
+
---
+++ assets/modules/evobackup/lang/italian.php
@@ -0,0 +1,69 @@
+<?php
+// ---------------------------------------------------------------
+// :: EvoBackup
+// ----------------------------------------------------------------
+//
+// 	Short Description:
+//         Backup Evo files and compress into .zip for easy download.
+//
+//   Version:
+//         1.2
+//
+//   Created by:
+// 	    Nicola Lambathakis http://www.tattoocms.it/
+//
+//
+// ----------------------------------------------------------------
+// :: Copyright & Licencing
+// ----------------------------------------------------------------
+//
+//   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
+//
+
+$_lang['modulename'] = 'EvoBackup';
+$_lang['backup'] = 'backup';
+//Manage Backups
+$_lang['manage_backup'] = 'Gestisci Backups';
+$_lang['manage_backup_descr'] = 'Scarica o cancella i precedenti backup';
+$_lang['backup_filename'] = 'Nome file';
+$_lang['backup_filesize'] = 'Dimensione';
+$_lang['backup_file_options'] = 'Opzioni';
+$_lang['delete_backup'] = 'Cancella Backup';
+$_lang['download_backup'] = 'Scarica Backup';
+//Generate Backup
+$_lang['generate_backup'] = 'Genera un nuovo archivio di Backup';
+$_lang['choose_backup'] = 'Scegli un tipo di Backup:';
+$_lang['light_backup'] = 'Backup Leggero';
+$_lang['medium_backup'] = 'Backup Medio';
+$_lang['full_backup'] = 'Backup Completo';
+//assets Backup
+$_lang['assets_backup'] = 'Assets Backup';
+$_lang['check_all'] = 'Seleziona tutto';
+$_lang['whole_assets'] = 'assets e tutte le sotto directory';
+$_lang['assets_user_folders'] = 'Directory Utente';
+$_lang['assets_elements_folders'] = 'Directory Elementi';
+$_lang['assets_system_folders'] = 'Directory di Sistema';
+//manager backup
+$_lang['manager_backup'] = 'Manager Backup';
+$_lang['whole_manager'] = 'manager e tutte le sotto directory';
+$_lang['only_those_manager_files'] = 'Solo questi file e directory:';
+$_lang['manager_config_file'] = 'file di configurazione';
+$_lang['manager_themes'] = 'temi del manager';
+//root backup
+$_lang['root_backup'] = 'Backup file in root';
+$_lang['root_backup_descr'] = 'Seleziona i file da aggiungere all\'archivio';
+//db backup
+$_lang['db_backup'] = 'Backup del Database';
+$_lang['include_db_to_zip'] = 'includi un backup .sql del database';
+// custom backup
+$_lang['custom_files_backup'] = 'Altri File e Cartelle';
+//buttons
+$_lang['backup_button_text'] = 'Backup!';
+$_lang['help'] = 'Aiuto';
+$_lang['settings_module'] = 'Configurazione';
+//help
+$_lang['help_light_backup'] = '<p>Questo backup include solo i file necessari, i file utente e il backup del db. <br/> Genera un archivio zip più piccolo e richiede meno memoria e risorse';
+$_lang['help_medium_backup'] = '<p>Questo backup include file necessari, i file utente, elementi (frammenti, moduli, plugin ..) e il backup del db. <br/> Genera un archivio zip di medie dimensioni</p> ';
+$_lang['help_full_backup'] = '<p>Questo backup include le directory assets e manager (incluse sottocartelle personalizzate), i file di root e il backup del db <br/> Genera un archivio zip più grande e richiede più memoria e risorse</p> </p>';
+
+
